728d7d70cc17fa0f20f79b7f2ffaaedf65bbe9b6
Ikrom <ikromyusupov09@gmail.com>
2022-06-05 23:52:38 +0500

    last one

--- logic20.py
+++ logic20.py
@@ -8,5 +8,27 @@ def main(n):
     Returns:
         bool: answer
     """
-
-    return
+    x1=n%10
+   
+    n//10
+    x2=n%10
+    
+    n//10
+    x3=n%10
+    
+    n//10
+    x4=n/n
+    
+    number_of_ones=0
+    number_of_zero=0
+    if x1==1 : number_of_ones+=1
+    if x1==0 : number_of_zero+=1
+    if x2==1 : number_of_ones+=1
+    if x2==0 : number_of_zero+=1
+    if x3==1 : number_of_ones+=1
+    if x3==0 : number_of_zero+=1
+    if x4==1 : number_of_ones+=1
+        
+    if number_of_ones>number_of_zero : return bool(1)
+    else : return bool(0)
+print(main(1110))
