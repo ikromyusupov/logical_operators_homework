e301049cae3cd81e061ecdb0e17e8d4b2a3f4bc0
Ikrom <ikromyusupov09@gmail.com>
2022-06-05 23:38:34 +0500

    polindrome

--- logic19.py
+++ logic19.py
@@ -8,4 +8,16 @@ def main(x):
     Returns:
         bool: answer
     """
-    return
+    x1 = a % 10
+    a //= 10
+
+    x2 = a % 10
+    a //= 10
+
+    x3 = a % 10
+    a //= 10
+
+    x4 = a % 10
+    a //= 10
+
+    return (x1 + x2) == (x3 + x4) and (x3 + x2) == (x1 + x4)
